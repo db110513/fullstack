f2ef2d81a0e3a5fe7af1469c883ad87968e0e2e3
DBG <danielbernardograset@gmail.com>
2024-11-20 14:26:40 +0100

    Frontend

--- flutterNode.js/ecommerce/frontend/lib/pantalles/inici.dart
+++ flutterNode.js/ecommerce/frontend/lib/pantalles/inici.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'registre.dart';
 
+
 class Inici extends StatelessWidget {
-  @override
+
+  final TextEditingController correuController = TextEditingController();
+  final TextEditingController contrassenyaController = TextEditingController();
+
+
+  void iniciarSessio(BuildContext context) async {
+
+    var url = Uri.parse('http://10.0.2.2:5000/api/usuari/inici');
+
+    var resposta = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode({
+        'correu': correuController.text,
+        'contrassenya': contrassenyaController.text,
+      }),
+    );
+
+    if (resposta.statusCode == 200) {
+      // Obté el token de la resposta
+      var dades = json.decode(resposta.body);
+      var token = dades['token'];
+    }
+    else {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Text('Error en iniciar sessió'),
+          );
+        },
+      );
+    }
+  }
+
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Inici de Sessió'),
+        centerTitle: true,
+        title: Text('Inici de Sessió', style: TextStyle(fontSize: 18)),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -14,21 +53,21 @@ class Inici extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextField(
+              controller: correuController,
               decoration: InputDecoration(
                 labelText: 'Correu electrònic',
               ),
             ),
             TextField(
+              controller: contrassenyaController,
               decoration: InputDecoration(
-                labelText: 'Contrasenya',
+                labelText: 'Contrassenya',
               ),
               obscureText: true,
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                // Aquí afegeix la lògica per fer login
-              },
+              onPressed: () => iniciarSessio(context),
               child: Text('Iniciar Sessió'),
             ),
             TextButton(
--- flutterNode.js/ecommerce/frontend/lib/pantalles/registre.dart
+++ flutterNode.js/ecommerce/frontend/lib/pantalles/registre.dart
@@ -1,38 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Registre extends StatelessWidget {
-  @override
+
+  final TextEditingController nomController = TextEditingController();
+  final TextEditingController correuController = TextEditingController();
+  final TextEditingController contrassenyaController = TextEditingController();
+
+
+  void registrar(BuildContext context) async {
+
+    var url = Uri.parse('http://10.0.2.2:5000/api/usuari/registre');
+    var resposta = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode({
+        'nom': nomController.text,
+        'correu': correuController.text,
+        'contrassenya': contrassenyaController.text,
+      }),
+    );
+
+    if (resposta.statusCode == 201) {
+      // Navegar a la pantalla d'inici de sessió després del registre
+      Navigator.pop(context);
+    }
+    else {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Text('Error en registrar l\'usuari'),
+          );
+        },
+      );
+    }
+  }
+
+
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Registrar-se'),
+        centerTitle: true,
+        title: Text('Registrar-se', style: TextStyle(fontSize: 18)),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: [
             TextField(
+              controller: nomController,
               decoration: InputDecoration(
                 labelText: 'Nom',
               ),
             ),
             TextField(
+              controller: contrassenyaController,
               decoration: InputDecoration(
-                labelText: 'Correu electrònic',
-              ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Contrasenya',
+                labelText: 'Contrassenya',
               ),
               obscureText: true,
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                // Aquí afegeix la lògica per crear un nou usuari
-              },
+              onPressed: () => registrar(context),
               child: Text('Registrar-se'),
             ),
           ],
