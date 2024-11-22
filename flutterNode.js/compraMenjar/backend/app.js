5570f58638922dddb059cf4f9d103cf46a72a454
DBG <danielbernardograset@gmail.com>
2024-11-22 18:04:27 +0100

    Backend

--- flutterNode.js/compraMenjar/backend/app.js
+++ flutterNode.js/compraMenjar/backend/app.js
@@ -1,23 +1,25 @@
 const express = require('express');
+const app = express();
+
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
 const menjarRoutes = require('./rutes/menjar');
 const comandaRoutes = require('./rutes/comandes');
-const rutesUsuaris = require('./rutes/usuaris');
+const usuarisRoutes = require('./rutes/usuaris');
 
-app.use('/api/usuaris', usuarisRoutes);
+const PORT = process.env.PORT || 3000;
 
-const app = express();
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors()); 
 
 mongoose.connect('mongodb://localhost:27017/compra-menjar')
   .then(() => console.log('Connexió a la base de dades correcta'))
   .catch(error => console.error('Error en connectar', error));
 
+app.use('/api/usuaris', usuarisRoutes);
 app.use('/api/menjar', menjarRoutes);
 app.use('/api/comandes', comandaRoutes);
 
-app.listen(3000, () => console.log('Servidor en funcionament al port 3000'));
+app.listen(PORT, () => console.log(`Servidor actiu al port ${PORT}`));
