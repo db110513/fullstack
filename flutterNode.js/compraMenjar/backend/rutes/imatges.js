f037cb18085e92133bbf5163fe95a36b60b210a9
DBG <danielbernardograset@gmail.com>
2024-11-26 13:11:08 +0100

    Puc pujar imatges a l'API des del HTML

--- flutterNode.js/compraMenjar/backend/rutes/imatges.js
+++ flutterNode.js/compraMenjar/backend/rutes/imatges.js
@@ -1,5 +1,7 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs'); 
+
 const express = require('express');
 const router = express.Router();
 
@@ -18,7 +20,16 @@ router.post('/pujar', upload.single('imatge'), (req, res) => {
   if (!req.file) {
     return res.status(400).send('No s\'ha pujat cap fitxer.');
   }
-  res.send(`Imatge pujada amb èxit! Nom del fitxer: ${req.file.originalname}`);
+
+  const filePath = path.join(__dirname, 'imatges', req.file.originalname);
+  
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (!err) {
+      return res.status(409).send(`Error: La imatge '${req.file.originalname}' ja existeix.`);
+    }
+
+    res.send(`Imatge pujada a l'API.\nNom del fitxer: ${req.file.originalname}`);
+  });
 });
 
 module.exports = router;
