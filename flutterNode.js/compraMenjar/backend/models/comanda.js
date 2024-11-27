e0bd42da74c1a57d4ac13ac8262060af9d41132c
DBG <danielbernardograset@gmail.com>
2024-11-27 14:43:50 +0100

    Modifico el get per obtindre comandes >   const comandes = await Comanda.find(); esborro menjarId

--- flutterNode.js/compraMenjar/backend/models/comanda.js
+++ flutterNode.js/compraMenjar/backend/models/comanda.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 
-
 const ComandaSchema = new mongoose.Schema({
   nomClient: { type: String, required: true },
   direccio: { type: String, required: true },
 });
-
 
 module.exports = mongoose.model('Comanda', ComandaSchema);
--- flutterNode.js/compraMenjar/backend/rutes/comandes.js
+++ flutterNode.js/compraMenjar/backend/rutes/comandes.js
@@ -4,15 +4,17 @@ const Comanda = require('../models/comanda');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const comandes = await Comanda.find().populate('menjarId');
+  const comandes = await Comanda.find();
   res.send(comandes);
 });
 
 router.post('/crea', async (req, res) => {
   try {
+    console.log('Dades rebudes de la comanda:', req.body);
     const novaComanda = new Comanda(req.body);
     await novaComanda.save();
     res.status(201).send(novaComanda);
+    
   } 
   catch (error) {
     res.status(400).send(error);
