88b676dd8973a5d90b98d79c9be9985a86affee6
DBG <danielbernardograset@gmail.com>
2024-12-03 10:58:14 +0100

    Modifico index.js

--- flutterNode.js/loTemps/backend/index.js
+++ flutterNode.js/loTemps/backend/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/BBDD')
+mongoose.connect('mongodb://localhost:27017/loTemps')
   .then(() => console.log('Connexió a la base de dades correcta'))
   .catch(error => console.error('Error en connectar', error));
 
