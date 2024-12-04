4be438bb74ada4b795c885fe115f7de5c8cc3763
DBG <danielbernardograset@gmail.com>
2024-12-04 18:39:25 +0100

    externaltzo funcions http per no sobrecarregar main thread

--- flutterNode.js/loTemps/frontend/lib/serveis/serveis_usuari.dart
+++ flutterNode.js/loTemps/frontend/lib/serveis/serveis_usuari.dart
@@ -1,11 +1,13 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter/material.dart';
 
 class ServeisUsuari {
 
   final String url = 'http://10.0.2.2:3000/usuaris';
 
-  Future<Map<String, dynamic>> login(String nomUsuari, String contrassenya) async {
+  Future<Map<String, dynamic>> login(String nomUsuari, String contrassenya, BuildContext context) async {
+
     final response = await http.post(
       Uri.parse('$url/login'),
       headers: {'Content-Type': 'application/json'},
@@ -15,13 +17,27 @@ class ServeisUsuari {
       }),
     );
 
-    if (response.statusCode == 200) return jsonDecode(response.body);
-    else throw Exception('Error en iniciar sessió');
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    }
+    else if (response.statusCode == 404) {
+      final responseData = jsonDecode(response.body);
 
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(responseData['message'] ?? 'Error desconegut'))
+      );
+
+      throw Exception(responseData['message']);
+    }
+    else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error en iniciar sessió'))
+      );
+      throw Exception('Error en iniciar sessió');
+    }
   }
 
-  Future<Map<String, dynamic>> registre(String nomUsuari, String contrassenya) async {
-
+  Future<Map<String, dynamic>> registre(String nomUsuari, String contrassenya, BuildContext context) async {
     final response = await http.post(
       Uri.parse('$url/registre'),
       headers: {'Content-Type': 'application/json'},
@@ -31,9 +47,14 @@ class ServeisUsuari {
       }),
     );
 
-    if (response.statusCode == 201) return jsonDecode(response.body);
-    else throw Exception('Error en registrar l\'usuari');
-
+    if (response.statusCode == 201) {
+      return jsonDecode(response.body);
+    }
+    else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error en registrar l\'usuari'))
+      );
+      throw Exception('Error en registrar l\'usuari');
+    }
   }
-
 }
