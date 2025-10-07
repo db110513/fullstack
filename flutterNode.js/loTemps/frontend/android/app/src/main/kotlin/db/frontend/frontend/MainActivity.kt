bab366a752021294830576a1fb397e72a08d3226
db110513 <127290046+db110513@users.noreply.github.com>
2025-10-07 12:35:23 +0200

    Delete flutterNode.js/loTemps directory

--- flutterNode.js/loTemps/frontend/android/app/src/main/kotlin/db/frontend/frontend/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package db.frontend.frontend
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- flutterNode.js/loTemps/readme.md
+++
@@ -1,4 +0,0 @@
-### Codi compilat:
-
-https://www.loom.com/share/143b9e12d5cf4f03be538c625eafd0c1
-
